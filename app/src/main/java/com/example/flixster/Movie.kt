d5ec1b7f74a9745c199ff715d5403f68811a7ff8
DrewH-NJIT <103283949+DrewH-NJIT@users.noreply.github.com>
2022-09-18 22:45:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -0,0 +1,29 @@
+package com.example.flixster
+
+import org.json.JSONArray
+
+data class Movie(
+    val movieId: Int,
+    private val posterPath: String,
+    val title: String,
+    val overview: String
+) {
+    val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+    companion object {
+        fun fromJsonArray(movieJSONArray: JSONArray) : List<Movie> {
+            val movies = mutableListOf<Movie>()
+            for (i in 0 until movieJSONArray.length()) {
+                val movieJSON = movieJSONArray.getJSONObject(i)
+                movies.add(
+                    Movie(
+                        movieJSON.getInt("id"),
+                        movieJSON.getString("poster_path"),
+                        movieJSON.getString("title"),
+                        movieJSON.getString("overview")
+                    )
+                )
+            }
+            return movies
+        }
+    }
+}
